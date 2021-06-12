ae0ef10b531f567cd6ab2899bbd02fc43b6eb85d
czarek009 <czarek.s2000@wp.pl>
2021-06-12 21:01:43 +0200

    data getter again (what the heck is wrong with me?)

--- data/data-getter.py
+++ data/data-getter.py
@@ -17,19 +17,28 @@ def data_getter(path, folder, fq, n, name="foobar"):
     pickle_out = open(join(folder, name+str(i)+".pickle"),"wb")
     qpa = []
     for ent in range(i, i+(n*fq), fq):
-      qpa.append(ent)
+      qpa.append(X[ent])
     pickle.dump(qpa, pickle_out)
     pickle_out.close()
 
 
-def data_getter(path, folder, fq, n, name="foobar"):
-  X = pickle.load(open(path, "rb"))
+def data_getter2(path_X, path_y, folder, fq, n, name="foobar"):
+  X = pickle.load(open(path_X, "rb"))
   X = np.asarray(X)
+  y = pickle.load(open(path_y, "rb"))
+  y = np.asarray(X)
 
   i = random.randint(0, len(X)-(n*fq))
-  pickle_out = open(join(folder, name+".pickle"),"wb")
+  pickle_out = open(join(folder, "X"+name+".pickle"),"wb")
   qpa = []
   for ent in range(i, i+(n*fq), fq):
-    qpa.append(ent)
+    qpa.append(X[ent])
+  pickle.dump(qpa, pickle_out)
+  pickle_out.close()
+
+  pickle_out = open(join(folder, "y"+name+".pickle"),"wb")
+  qpa = []
+  for ent in range(i, i+(n*fq), fq):
+    qpa.append(y[ent])
   pickle.dump(qpa, pickle_out)
   pickle_out.close()
