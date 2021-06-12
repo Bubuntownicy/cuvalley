adb91f9dec86e4b5248c7634b2bed397305c1279
Cezary Troska <CTroska@noblesys.com>
2021-06-12 19:56:13 +0200

    Rewrite LSTM nn

--- furnace-simulator/RNN.py
+++ furnace-simulator/RNN.py
@@ -1,8 +1,9 @@
-import numpy as np
 import pickle
-import tensorflow as tf
+
+import numpy as np
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.layers import Dense, LSTM
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dense, SimpleRNN, Input, Embedding, GRU, Flatten, LSTM
 
 X = pickle.load(open("X_19042021.pickle", "rb"))
 y = pickle.load(open("Y_19042021.pickle", "rb"))
@@ -11,28 +12,29 @@ y = list(map(float, y))
 X = np.asarray(X)
 y = np.asarray(y)
 
-#print(len(y))
-#print(len(X))
 
-#print(X.shape)
-#X = X/255.0
+def train_nn(time_steps=400, observations=1, test_percent=0.1, epochs=510, batch_size=512):
+    pointbreak = X.size * test_percent - (X.size * test_percent % (time_steps * observations))
+    X_train = X[pointbreak:]
+    y_train = y[pointbreak:]
+    X_test = X[:pointbreak]
+    y_test = y[:pointbreak]
 
-X_train = X[10000:]
-y_train = y[10000:]
-X_test = X[:10000]
-y_test = y[:10000]
+    X_train = X_train.reshape(-1, time_steps, observations)
+    y_train = y_train.reshape(-1, time_steps, observations)
+    X_test = X_test.reshape(-1, time_steps, observations)
+    y_test = y_test.reshape(-1, time_steps, observations)
 
-X_train = X_train.reshape(2351, 1, 400)
-y_train = y_train.reshape(2351, 1, 400)
-X_test  = X_test.reshape(25, 1, 400)
-y_test = y_test.reshape(25, 1, 400)
+    checkpoint_path = "furnace.ckpt"
+    ckpt_callback = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_freq='epoch', period=10)
 
-model = Sequential()
-model.add(LSTM(512, activation='relu', kernel_initializer='he_normal', input_shape=(1, 400)))
-model.add(Dense(32, activation='relu', kernel_initializer='he_normal'))
-model.add(Dense(1))
-# compile the model
-model.compile(optimizer='adam', loss='mse', metrics=['mae'])
-model.summary()
-# fit the model
-model.fit(X_train, y_train, epochs=512, batch_size=256, verbose=2, validation_data=(X_test, y_test))
+    model = Sequential()
+    model.add(LSTM(64, input_shape=(time_steps, observations)))
+    model.add(Dense(32))
+    model.add(Dense(1))
+    # compile the model
+    model.compile(optimizer='adam', loss='mse', metrics=['mae'])
+    model.summary()
+    # fit the model
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=1, validation_data=(X_test, y_test),
+              callbacks=[ckpt_callback])
