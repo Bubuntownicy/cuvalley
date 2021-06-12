3ddb6f5da715d5f8323caecc1b320657d6232c9d
czarek009 <czarek.s2000@wp.pl>
2021-06-12 20:37:41 +0200

    Fixing data-getter script (I hope so)

--- data/data-getter.py
+++ data/data-getter.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 from os.path import join
 from tqdm import tqdm as tqdm
+import random
 
 # path - ścieżka do pliku *.pickle z którego pobiera dane
 # folder - folder do którego zapisuje pliki wyjściowe
@@ -12,10 +13,23 @@ def data_getter(path, folder, fq, n, name="foobar"):
   X = pickle.load(open(path, "rb"))
   X = np.asarray(X)
 
-  for i in tqdm(range(0, len(X), fq)):
+  for i in tqdm(range(0, len(X)-(n*fq), fq)):
     pickle_out = open(join(folder, name+str(i)+".pickle"),"wb")
     qpa = []
-    for ent in range(0, len(X)-(n*fq), fq):
+    for ent in range(i, i+(n*fq), fq):
       qpa.append(ent)
     pickle.dump(qpa, pickle_out)
     pickle_out.close()
+
+
+def data_getter(path, folder, fq, n, name="foobar"):
+  X = pickle.load(open(path, "rb"))
+  X = np.asarray(X)
+
+  i = random.randint(0, len(X)-(n*fq))
+  pickle_out = open(join(folder, name+".pickle"),"wb")
+  qpa = []
+  for ent in range(i, i+(n*fq), fq):
+    qpa.append(ent)
+  pickle.dump(qpa, pickle_out)
+  pickle_out.close()
