870515bcad7d2b50c135e23894feba2a856938f5
AntoniDabrowski <dabrowski.ant@gmail.com>
2021-06-12 20:13:01 +0200

    Remove unimportant file.

--- MCTS/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import random
-
-if __name__ == '__main__':
-    to_rollout = 100
-    prev = [10,10,10,10]
-    end = [np.random.randint(0,9) for _ in range(to_rollout)]
-    print(prev+end)
