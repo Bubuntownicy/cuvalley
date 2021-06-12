b90bcbefc1883736a7c09e063818a953eff5d1cd
czarek009 <czarek.s2000@wp.pl>
2021-06-12 17:05:17 +0200

    RNN fail

--- data_for_simulation/pickle_maker.py
+++ data_for_simulation/pickle_maker.py
@@ -13,7 +13,8 @@ x = []
 with open(x_file_name, newline='') as x_csvfile:
     x_reader = csv.DictReader(x_csvfile)
     for row in x_reader:
-        x.append(row['001FCx00285_SPPV.PV'])
+        if (row['001FCx00285_SPPV.PV'] != '001FCx00285_SPPV.PV'):
+            x.append(row['001FCx00285_SPPV.PV'])
 
 x_name_pickle = 'X_' + date + '.pickle'
 pickle_out = open(x_name_pickle, "wb")
@@ -25,7 +26,8 @@ y = []
 with open(y_file_name, newline='') as y_csvfile:
     y_reader = csv.DictReader(y_csvfile)
     for row in y_reader:
-        y.append(row['001NIR0SZR0.daca.pv'])
+        if (row['001NIR0SZR0.daca.pv'] != '001NIR0SZR0.daca.pv'):
+            y.append(row['001NIR0SZR0.daca.pv'])
 
 y_name_pickle = 'Y_' + date + '.pickle'
 pickle_out = open(y_name_pickle, "wb")
--- furnace-simulator/RNN.py
+++ furnace-simulator/RNN.py
@@ -2,47 +2,37 @@ import numpy as np
 import pickle
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dense, SimpleRNN, InputLayer, Embedding, GRU
+from tensorflow.keras.layers import Activation, Dense, SimpleRNN, Input, Embedding, GRU, Flatten, LSTM
 
-'''
-X = pickle.load(open("X.pickle", "rb"))
-y = pickle.load(open("y.pickle", "rb"))
+X = pickle.load(open("X_19042021.pickle", "rb"))
+y = pickle.load(open("Y_19042021.pickle", "rb"))
+X = list(map(float, X))
+y = list(map(float, y))
 X = np.asarray(X)
 y = np.asarray(y)
 
-X = X/255.0
+#print(len(y))
+#print(len(X))
 
-X_train = X[128:]
-y_train = y[128:]
-X_test = X[:128]
-y_test = y[:128]
-'''
+#print(X.shape)
+#X = X/255.0
+
+X_train = X[10000:]
+y_train = y[10000:]
+X_test = X[:10000]
+y_test = y[:10000]
+
+X_train = X_train.reshape(2351, 1, 400)
+y_train = y_train.reshape(2351, 1, 400)
+X_test  = X_test.reshape(25, 1, 400)
+y_test = y_test.reshape(25, 1, 400)
 
 model = Sequential()
-model.add(Embedding(input_dim=600, output_dim=600))
-
-# The output of GRU will be a 3D tensor of shape (batch_size, timesteps, 256)
-model.add(GRU(256, return_sequences=True))
-
-# The output of SimpleRNN will be a 2D tensor of shape (batch_size, 128)
-model.add(SimpleRNN(128))
-
+model.add(LSTM(512, activation='relu', kernel_initializer='he_normal', input_shape=(1, 400)))
+model.add(Dense(32, activation='relu', kernel_initializer='he_normal'))
 model.add(Dense(1))
-
+# compile the model
+model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 model.summary()
-
-
-model.compile(optimizer = "adam", 
-              loss = tf.keras.losses.BinaryCrossentropy(),
-              metrics = ["accuracy"])
-
-'''
-model.fit(X_train, y_train, 
-          validation_data=(X_test, y_test),
-          batch_size=32, epochs=8, verbose=1)
-
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print("\nTest accuracy: ", test_acc)
-
-model.save('saved_model')
-'''
+# fit the model
+model.fit(X_train, y_train, epochs=512, batch_size=256, verbose=2, validation_data=(X_test, y_test))
